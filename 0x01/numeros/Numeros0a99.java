f586e217ccfae1a0c8e00c7613d0df84d610cf4c
Ana Carolina Ferreira Rocha Frugis Fasanel <CIANDT\anaf@lnb024399cps.ciandt.global>
2022-03-13 10:54:23 -0300

    resolucao exe 0

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,10 @@
+package numeros;
+
+public class Numeros0a99 {
+    public static void main(String[] args) {
+        for (int i=0; i<99; i++){
+            System.out.printf("%d, ", i);
+        }
+        System.out.printf("99\n");
+    }
+}
