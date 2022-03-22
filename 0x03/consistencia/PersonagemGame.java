90b987d16e354ccce2017d9a7ea6d248fab8b451
Ana Carolina Ferreira Rocha Frugis Fasanel <CIANDT\anaf@lnb024399cps.ciandt.global>
2022-03-22 16:50:13 -0300

    resolucao exe 4

---
+++ 0x03/consistencia/PersonagemGame.java
@@ -0,0 +1,52 @@
+public class PersonagemGame {
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        this.setSaudeAtual(saudeAtual);
+        this.nome = nome;
+    }
+
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (saudeAtual == 0){
+            this.status = "morto";
+        } else {
+            this.status = "vivo";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if ( nome != null && !nome.isEmpty()){
+            this.nome = nome;
+        }
+    }
+
+    public void tomarDano (int quantidadeDeDano){
+        if (this.getSaudeAtual()-quantidadeDeDano < 0){
+            this.setSaudeAtual(0);
+        }else{
+            this.setSaudeAtual(this.getSaudeAtual()-quantidadeDeDano);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura){
+        this.setSaudeAtual(this.getSaudeAtual()+quantidadeDeCura);
+        if (saudeAtual > 100){
+            this.saudeAtual = 100;
+        }
+    }
+}
